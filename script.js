78a8395aa4d2642388ff478331aad76951ceef30
Veyg <dawid.grochowski@veyg.me>
2023-10-09 14:03:33 +0200

    First good redesign

--- script.js
+++ script.js
@@ -1,17 +1,47 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const aboutMeText = "I'm a 24-year-old IT student who is passionate about creating innovative solutions through writing apps and learning from them.";
-  
-    const aboutMeElement = document.getElementById('aboutMe');
-    let aboutMeIndex = 0;
-  
-    function typeAboutMe() {
-      aboutMeElement.innerHTML = aboutMeText.slice(0, aboutMeIndex);
-      aboutMeIndex++;
-      if (aboutMeIndex <= aboutMeText.length) {
-        setTimeout(typeAboutMe, 50); // Adjust the typing speed if needed
+// Smooth scrolling for anchor links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+      e.preventDefault();
+      const target = document.querySelector(this.getAttribute('href'));
+      if (target) {
+          target.scrollIntoView({
+              behavior: 'smooth'
+          });
       }
-    }
-  
-    typeAboutMe();
   });
-  
+});
+
+// Age calculation based on birth date
+document.addEventListener("DOMContentLoaded", function() {
+  const birthDate = new Date("1998-12-19");
+  const currentDate = new Date();
+  const age = currentDate.getFullYear() - birthDate.getFullYear() - 
+              (currentDate.getMonth() < birthDate.getMonth() || 
+              (currentDate.getMonth() === birthDate.getMonth() && currentDate.getDate() < birthDate.getDate()) ? 1 : 0);
+  const ageElement = document.getElementById("age");
+  if (ageElement) {
+      ageElement.textContent = age;
+  }
+});
+const modal = document.getElementById('projectModal');
+const closeBtn = document.querySelector('.close-btn');
+
+// Open modal when a project card is clicked
+document.querySelectorAll('.project-card').forEach(card => {
+    card.addEventListener('click', () => {
+        modal.style.display = 'block';
+        // You can also populate the modal with specific project details here if needed
+    });
+});
+
+// Close modal when the close button is clicked
+closeBtn.addEventListener('click', () => {
+    modal.style.display = 'none';
+});
+
+// Close modal when clicking outside the modal content
+window.addEventListener('click', (event) => {
+    if (event.target === modal) {
+        modal.style.display = 'none';
+    }
+});
